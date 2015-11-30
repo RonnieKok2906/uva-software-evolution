d8bb9253237fa29c81119cbdc2b0c40bedfee501
Ton Heijligers <tonheijligers@gmail.com>
2015-11-30 14:34:24 +0100

    add Util class.

---
+++ testCloneSource/src/clonePackage/Util.java
@@ -0,0 +1,20 @@
+package clonePackage;
+
+public class Util {
+
+	static public void foo(int d1, int d2)
+	{
+	}
+	
+	static public void foo(double d1, double d2)
+	{
+	}
+	
+	static public void foo(double d)
+	{
+	}
+	
+	static public void foo(double d1, double d2, int i)
+	{
+	}
+}
